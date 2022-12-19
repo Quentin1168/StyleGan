9bbe469d1f0a16fd56999fbb81142096d8ed10bc
Quentin Chang <quentinchang1168@gmail.com>
2022-12-19 18:31:21 +1000

    finished and tested dataloader file

---
+++ dataset.py
@@ -0,0 +1,32 @@
+from torch.utils.data import Dataset
+import torchvision
+from torchvision.transforms import transforms
+import torch.cuda
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+
+class DataLoader(Dataset):
+    
+    
+    def __init__(self, path):
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize((0.5,), (0.5,)),
+                                        ])
+        
+        dataloader = torchvision.datasets.ImageFolder(root = path, 
+                                                       transform = transform)
+        
+        self.data = dataloader
+        
+    def __len__(self):
+        return len(self.data)
+        
+                
+
+    #returns the data_loader object
+    def __getitem__(self, idx):
+        return self.data[idx]
+        
