4c0c9622fd972536bb2b44f6c07e911dd5e5ac74
Quentin Chang <quentinchang1168@gmail.com>
2023-01-16 21:59:39 +1000

    added a few more functionalities for synthesis network

--- modules.py
+++ modules.py
@@ -3,7 +3,7 @@ import torchvision
 from torchvision.transforms import transforms
 import torch.cuda
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 """
 Adaptive Instance Normalisation (AdaIN)
@@ -55,10 +55,95 @@ class adaIN(nn.Module):
        
        return std
 
-    def forward(self, x, y):
-        return (self.spatial_std(y)*((x.permute([2,3,0,1]) 
+    def forward(self, x, y_scale, y_shift):
+        return (y_scale*((x.permute([2,3,0,1]) 
                                       - self.spatial_mean(x))/
                                      self.spatial_std(x)) \
-                + self.spatial_mean(y)).permute([2,3,0,1])
+                + y_shift).permute([2,3,0,1])
+    
+
+class z_to_w(nn.Module):
+    
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        
+        self.z_to_w = nn.Sequential(FCLayer(in_ch, out_ch),
+                                    nn.ReLU(),
+                                    FCLayer(in_ch, out_ch),
+                                    nn.ReLU(),
+                                    FCLayer(in_ch, out_ch),
+                                    nn.ReLU(),
+                                    FCLayer(in_ch, out_ch),
+                                    nn.ReLU(),
+                                    FCLayer(in_ch, out_ch),
+                                    nn.ReLU(),
+                                    FCLayer(in_ch, out_ch),
+                                    nn.ReLU(),
+                                    FCLayer(in_ch, out_ch),
+                                    nn.ReLU(),)
+                                    
+        
+    def forward(self, x):
+        return self.z_to_w(x)
+    
+class A(nn.Module):
+    
+    def __init__(self, in_ch, out_ch, hidn_ch):
+        super().__init__()
+        
+        self.scale = nn.Linear(in_ch, hidn_ch)
+        self.shift = nn.Linear(in_ch, out_ch)
+        
+    def forward(self, x):
+        scale = self.scale(x)
+        shift = self.shift(x)
+        
+        return scale, shift
+
+class FCLayer(nn.Module):
+    
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        
+        self.FC = nn.Linear(in_ch, out_ch)
+        
+        #set normal distribution N(0, 1)
+        self.FC.weight.data.normal_()
+        self.FC.bias.data.zero_()
+        
+    def forward(self, x):
+        return self.FC(x)
+
+
+class SynthLayer(nn.Module):
+    
+    def __init__(self, in_ch, out_ch, hidn_ch, scale = True, first = False):
+        super().__init__()
+
+        conv_list = []
+        if first == True:
+            self.conv1 = nn.Conv2d(in_ch, in_ch, kernel_size=3, 
+                                   stride=1, padding=1)
+            self.conv2 = nn.Conv2d(in_ch, in_ch, kernel_size=3,
+                               stride=1, padding=1)
+            conv_list.append(self.conv1)
+            conv_list.append(self.conv2)
+        else:
+            self.conv1 = nn.Conv2d(in_ch, hidn_ch, kernel_size=3, 
+                               stride=1, padding=1)
+            self.conv2 = nn.Conv2d(hidn_ch, out_ch, kernel_size=3,
+                               stride=1, padding=1)
+            conv_list.append(self.conv1)
+            conv_list.append(self.conv2)
+        self.scale = scale
+        self.gen_noise = torch.randn_like
+        self.upsample = F.interpolate #learnt upsample
+        self.noise_factor = 0.01
+        
     
     
+        
+        
+        
+    
+        
