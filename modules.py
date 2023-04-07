19842b06d9c46e824a28d49a04c0ec4b23cfcdd1
Quentin Chang <quentinchang1168@gmail.com>
2023-04-07 14:40:21 +1000

    refactored code structure to include ProGAN features, simplified some functions

--- modules.py
+++ modules.py
@@ -5,215 +5,226 @@ import torch.cuda
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Generator(nn.Module):
+
+factors = [1,1,1,1/2,1/4,1/8,1/16,1/32]
+
+
+class WS(nn.Module):
+    
+    def __init__(self, layer):
+        super(WS, self).__init__()
+        
+        self.layer = layer
+        
+        if isinstance(self.layer, nn.Conv2d):
+            self.scale = (2 / (self.layer.in_channels * (self.layer.kernel_size ** 2))) ** 0.5
+        else:
+            self.scale = (2 / self.layer.in_channels) ** 0.5
+        
+        self.bias = self.layer.bias
+        self.layer.bias = None
+        
+        nn.init.normal_(self.layer.weight)
+        nn.init.zeros_(self.bias)
+        
+
+    def forward(self, x):
+        
+        if isinstance(self.layer, nn.Conv2d):
+            return self.layer(x * self.scale) + self.bias.view(1, self.bias.shape[0], 1, 1)
+        else:
+            return self.layer(x*self.scale) + self.bias
+        
+        
+class PixelNorm(nn.Module):
+    def __init__(self):
+        super(PixelNorm, self).__init__()
+        self.epsilon = 1e-8
+        
+    def forward(self, x):
+        div = torch.sqrt(torch.mean(x ** 2, dim =1, keepdim = True) + self.epsilon)
+        return x / div
+
+            
+        
+"""
+Adaptive Instance Normalisation (AdaIN)
+
+Takes in a content input x and style input y, and algins the mean and variance
+of the content input to that of the style's in a per channel basis.
+
+AdaIN is used in StyleGan due to the significant improvement in performance 
+when transferring styles. 
+
+"""
+class adaIN(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+    
+        
+    """
+    Instance Normalisation normalises independent of sample and channel.
+    Based on equation 5 and 6 of the paper.
+    """
+    def spatial_mean(self, x):
+        
+        #sum the values along the height and width (spatial) dimensions
+        spatial_sum = torch.sum(x, (2,3))
+        
+        #total size of the tensor, height * width
+        denom = x.size(2) * x.size(3)
+        
+        return spatial_sum/denom
+   
+    
+    def spatial_std(self, x):
+       spatial_mean = self.spatial_mean(x)
+       #epsilon is a hyperparamter to avoid dividing by 0
+       e = 1e-9 
+       #broadcast to subtract mean tensor and input
+       
+       #need to permute to put H, W in front to broadcast
+       
+       x = x.permute([2,3,0,1])
+       x = x - spatial_mean
+       
+       #revert after broadcast
+       x = x.permute([2,3,0,1])
+       denom = x.size(2) * x.size(3)
+       
+       std = torch.sqrt((torch.sum(x, (2,3))**2 + e)/denom)
+       
+       return std
+
+    def forward(self, x, y):
+        return (self.spatial_std(y)*((x.permute([2,3,0,1]) 
+                                      - self.spatial_mean(x))/
+                                     self.spatial_std(x)) \
+                + self.spatial_mean(y)).permute([2,3,0,1])
+    
+
+class z_to_w(nn.Module):
     
     def __init__(self, in_ch, out_ch):
         super().__init__()
-        self.count = 0
-        self.first = self.SynthLayer(in_ch, out_ch, True, True)
-        self.synth_list = []
-        self.z_w = self.z_to_w(in_ch, out_ch)
-        self.const = torch.zeros(in_ch)
+        self.pixel_norm = PixelNorm() 
+        self.z_to_w = nn.Sequential(WS(nn.Linear(in_ch, out_ch)),
+                                    nn.ReLU(),
+                                    WS(nn.Linear(in_ch, out_ch)),
+                                    nn.ReLU(),
+                                    WS(nn.Linear(in_ch, out_ch)),
+                                    nn.ReLU(),
+                                    WS(nn.Linear(in_ch, out_ch)),
+                                    nn.ReLU(),
+                                    WS(nn.Linear(in_ch, out_ch)),
+                                    nn.ReLU(),
+                                    WS(nn.Linear(in_ch, out_ch)),
+                                    nn.ReLU(),
+                                    WS(nn.Linear(in_ch, out_ch)),
+                                    nn.ReLU())
+                                    
+        
+    def forward(self, x):
+        normalized = self.pixel_norm(x)
+        return self.z_to_w(normalized)
+
+
+class SynthLayer(nn.Module):
     
-    def add_block(self):
-        self.count += 1
-        mult = self.synth_list[-1].get_out_ch()
-        new_block = self.SynthLayer(mult, 2 * mult, True, False)
-        self.synth_list.append(new_block)
+    def __init__(self, in_ch, out_ch, hidn_ch, scale = True, first = False):
+        super().__init__()
+        self.out_ch = out_ch
+        self.first = first
+              
+        self.conv1 = WS(nn.Conv2d(in_ch, hidn_ch, kernel_size=3, 
+                           stride=1, padding=1))
+        self.conv2 = WS(nn.Conv2d(hidn_ch, out_ch, kernel_size=3,
+                           stride=1, padding=1))
+
+        self.scale = scale
+        self.upsample = F.interpolate #learnt upsample
+        self.noise_factor = 0.01
         
+        self.adaIN1 = self.adaIN()
+        self.adaIN2 = self.adaIN()
+ 
+    def forward(self, w, x):
         
-    def forward(self):
-        x = self.first(self.const)
-        for i in self.synth_list:
-            x = i(x)
+        if self.first == False:
+            x = nn.ReLU(self.conv1(x))
+            
+            x = x + nn.Parameter(torch.zeros(1, self.out_ch, 1, 1)) + \
+                torch.randn((x.shape[0], 1, x.shape[2], x.shape[3]))
+            x = self.adaIN1(x,w)
+            
+            x = nn.ReLU(self.conv2(x))
+            x = x + nn.Parameter(torch.zeros(1, self.out_ch, 1, 1)) + \
+                torch.randn((x.shape[0], 1, x.shape[2], x.shape[3]))
+            x = self.adaIN2(x,w)
+        
+        else:
+            x = x + nn.Parameter(torch.zeros(1, self.out_ch, 1, 1)) + \
+                torch.randn((x.shape[0], 1, x.shape[2], x.shape[3]))
+            x = self.adaIN1(x,w)
+            x = nn.ReLU(self.conv1(x))
+            x = x + nn.Parameter(torch.zeros(1, self.out_ch, 1, 1)) + \
+                torch.randn((x.shape[0], 1, x.shape[2], x.shape[3]))
+           
+            
         return x
-            
+
         
-    """
-    Adaptive Instance Normalisation (AdaIN)
+        
+class Generator(nn.Module):
     
-    Takes in a content input x and style input y, and algins the mean and variance
-    of the content input to that of the style's in a per channel basis.
+    def __init__(self, z_dim, w_dim, in_ch, img_ch = 3):
+        super().__init__()
+        
+        self.map = z_to_w(z_dim, w_dim)
+        self.rgb = WS(nn.Conv2d(in_ch, img_ch, kernel_size = 1, stride = 1, 
+                                padding = 0))
+        
+        self.prog_blocks = nn.ModuleList([SynthLayer(in_ch, in_ch, in_ch, 
+                                                     first = True)])
+        
+        self.rgb_layers = nn.ModuleList([self.rgb])
+        
+        self.init_constant = nn.Parameter(torch.ones(1, in_ch, 4,4))
+        
+        
+        #initiate an empty untrained list of blocks
+        for i in range(len(factors)-1):
+            conv_in_c = int(in_ch * factors[i])
+            conv_out_c = int(in_ch * factors[i+1])
+            
+            self.prog_blocks.append(SynthLayer(conv_in_c, conv_out_c, 
+                                               conv_out_c))
+            self.rgb_layers.append(WS(nn.Conv2d(conv_in_c, img_ch, 
+                                                kernel_size = 1, stride = 1, 
+                                                padding = 0)))
+        
+    def fade_in(self, alpha, upscaled, generated):
+        return torch.tanh(alpha * generated + (1-alpha ) * upscaled)
     
-    AdaIN is used in StyleGan due to the significant improvement in performance 
-    when transferring styles. 
-    
-    """
-    class adaIN(nn.Module):
+    def forward(self, noise, alpha, steps):
+        w = self.map(noise)
+        up = self.prog_blocks[0](self.init_constant, w)
+        x = adaIN()(up, w)
+        if steps == 0:
+            up = self.rgb_layers[steps](up)
+            return up
         
-        def __init__(self):
-            super().__init__()
+        for step in range(steps):
+            up = F.interpolate(x, scale_factor =2, mode = 'bilinear')
+            x = self.prog_blocks[step](up, w)
+            
+        up_final = self.rgb_layers[steps-1](up)
+        out_final = self.rgb_layers[steps](x)
         
-            
-        """
-        Instance Normalisation normalises independent of sample and channel.
-        Based on equation 5 and 6 of the paper.
-        """
-        def spatial_mean(self, x):
-            
-            #sum the values along the height and width (spatial) dimensions
-            spatial_sum = torch.sum(x, (2,3))
-            
-            #total size of the tensor, height * width
-            denom = x.size(2) * x.size(3)
-            
-            return spatial_sum/denom
-       
-        
-        def spatial_std(self, x):
-           spatial_mean = self.spatial_mean(x)
-           #epsilon is a hyperparamter to avoid dividing by 0
-           e = 1e-9 
-           #broadcast to subtract mean tensor and input
-           
-           #need to permute to put H, W in front to broadcast
-           
-           x = x.permute([2,3,0,1])
-           x = x - spatial_mean
-           
-           #revert after broadcast
-           x = x.permute([2,3,0,1])
-           denom = x.size(2) * x.size(3)
-           
-           std = torch.sqrt((torch.sum(x, (2,3))**2 + e)/denom)
-           
-           return std
-    
-        def forward(self, x, y):
-            return (self.spatial_std(y)*((x.permute([2,3,0,1]) 
-                                          - self.spatial_mean(x))/
-                                         self.spatial_std(x)) \
-                    + self.spatial_mean(y)).permute([2,3,0,1])
-        
-    
-    class z_to_w(nn.Module):
-        
-        def __init__(self, in_ch, out_ch):
-            super().__init__()
-            
-            self.z_to_w = nn.Sequential(self.FCLayer(in_ch, out_ch),
-                                        nn.ReLU(),
-                                        self.FCLayer(in_ch, out_ch),
-                                        nn.ReLU(),
-                                        self.FCLayer(in_ch, out_ch),
-                                        nn.ReLU(),
-                                        self.FCLayer(in_ch, out_ch),
-                                        nn.ReLU(),
-                                        self.FCLayer(in_ch, out_ch),
-                                        nn.ReLU(),
-                                        self.FCLayer(in_ch, out_ch),
-                                        nn.ReLU(),
-                                        self.FCLayer(in_ch, out_ch),
-                                        nn.ReLU(),)
-                                        
-            
-        def forward(self, x):
-            std, mean = torch.std_mean(x);
-            norm = transforms.Compose([
-                transforms.Normalize(mean, std)])
-            normalized = norm(x)
-            return self.z_to_w(normalized)
-        
-    class A(nn.Module):
-        
-        def __init__(self, in_ch, out_ch, hidn_ch):
-            super().__init__()
-            
-            self.scale = nn.Linear(in_ch, hidn_ch)
-            self.shift = nn.Linear(in_ch, out_ch)
-            
-        def forward(self, x):
-            scale = self.scale(x)
-            shift = self.shift(x)
-            
-            return scale, shift
-    
-    class FCLayer(nn.Module):
-        
-        def __init__(self, in_ch, out_ch):
-            super().__init__()
-            
-            self.FC = nn.Linear(in_ch, out_ch)
-            
-            #set normal distribution N(0, 1)
-            self.FC.weight.data.normal_()
-            self.FC.bias.data.zero_()
-            
-        def forward(self, x):
-            return self.FC(x)
+        return self.fade_in(alpha, up_final, out_final)
     
     
-    class SynthLayer(nn.Module):
-        
-        def __init__(self, in_ch, out_ch, hidn_ch, scale = True, first = False):
-            super().__init__()
-            
-            self.first = first
-                  
-            self.conv1 = nn.Conv2d(in_ch, hidn_ch, kernel_size=3, 
-                               stride=1, padding=1)
-            self.conv2 = nn.Conv2d(hidn_ch, out_ch, kernel_size=3,
-                               stride=1, padding=1)
-    
-            self.scale = scale
-            self.gen_noise = torch.randn_like
-            self.upsample = F.interpolate #learnt upsample
-            self.noise_factor = 0.01
-            
-            self.adaIN1 = self.adaIN()
-            self.adaIN2 = self.adaIN()
-        
-        def get_out_ch(self):
-            return self.out_ch
-        
-        def forward(self, w1, w2, x):
-            
-            if self.first == False:
-                A1 = self.A(w1.shape[0], w1.shape[0], x.shape[0])
-                w1 = A1(w1)
-                x = self.conv1(x)
-                x = x + self.gen_noise
-                x = self.adaIN1(x,w1)
-                
-                x = self.conv2(x)
-                x = x + self.gen_noise
-                A2 = self.A(w2.shape[0], w2.shape[0], x.shape[0])
-                w2 = A2(w2)
-                x = self.adaIN2(x,w2)
-            
-            else:
-                A1 = self.A(w1.shape[0], w1.shape[0], x.shape[0])
-                w1 = A1(w1)
-                x = x + self.gen_noise
-                x = self.adaIN1(x,w1)
-                x = self.conv1(x)
-                x = x + self.gen_noise
-                A2 = self.A(w2.shape[0], w2.shape[0], x.shape[0])
-                w2 = A2(w2)
-                x = self.adaIN2(x, w2)
-            return x
-    
-    class MainStruct(nn.Module):
-        
-        def __init__(self, in_ch, res):
-            super().__init__()
-            self.in_ch = in_ch
-            self.res = res
-            self.layers = []
-            
-            self.layers.append(Generator.SynthLayer(self.in_ch, self.in_ch, self.in_ch*2, first = True))
-            self.ex_ch =self.in_ch*2
-           # for i in range(0, self.res):
-                
-            #    self.layers.append(Generator.SynthLayer(ex_ch, ex_ch, ex_ch*2))
-            #    ex_ch = ex_ch * 2
-            
-            self.mapping = Generator.z_to_w(512, 512)
-        
-        def add_block(self):
-            self.layers.append(Generator.SynthLayer(self.ex_ch, self.ex_ch, self.ex_ch *2))
-            self.ex_ch *= 2
-        
 class Discriminator(nn.Module):
     
     def __init__(self, in_ch, out_ch, hidn_ch, alpha):
